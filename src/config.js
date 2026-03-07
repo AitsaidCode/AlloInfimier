742e83e1688afea6b9cf577852bc49c759d3a423
Owner <owner@Hichams-MacBook-Pro.local>
2026-03-07 20:41:59 +0000

    chore: update contact email

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@
 
 export const PHONE = '+212661000000';
 export const WHATSAPP = '212661000000';
-export const EMAIL = 'contact@alloinfirmier.ma';
+export const EMAIL = 'aitsaid.hicham.98@gmail.com';
